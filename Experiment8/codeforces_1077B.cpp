89f4acefacc86e4e3ea0dd5bce2e6bdb76acd167
Diwansh <diwanshbakshi2134@gmail.com>
2026-04-26 11:37:43 +0530

    Create codeforces_1077B.cpp

---
+++ Experiment8/codeforces_1077B.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n];
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    int count = 0;
+
+    for(int i = 1; i < n - 1; i++) {
+        if(a[i-1] == 1 && a[i] == 0 && a[i+1] == 1) {
+            count++;
+            i += 2;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
